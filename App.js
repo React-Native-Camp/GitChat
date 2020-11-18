e39030ab54633881d086ce97db235094e4acbb1d
Rawan Al-Rajabi <rawanalrajabi2016@gmail.com>
2020-11-18 19:11:45 +0200

    Initial project files

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  Text
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return <Text>Hello</Text>;
+};
+
+export default App;
