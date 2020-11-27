f6c86716fbb4e65cd2135f432f265c9c1fd0560a
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-11-27 18:28:20 +0200

    create bottom tab navigation with initial style #16

---
+++ client/src/components/Contacts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Contacts() {
+  return (
+    <View>
+      <Text> it's contact</Text>
+    </View>
+  );
+}
+
+export default Contacts;
---
+++ client/src/components/Messages.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Messages() {
+  return (
+    <View>
+      <Text> it's Chat</Text>
+    </View>
+  );
+}
+
+export default Messages;
