686b8183073d3a7a1ee9db3547fbf6e3f5b93d34
Rawan Al-Rajabi <rawanalrajabi2016@gmail.com>
2020-12-01 14:31:28 +0200

    Created app's main page

---
+++ client/src/screens/AppMainPage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './../../components/Header';
+import TabNavigator from './../../components/TabNavigator';
+
+const AppMainPage = () => {
+  return (
+    <>
+      <Header />
+      <TabNavigator />
+    </>
+  );
+};
+
+export default AppMainPage;
