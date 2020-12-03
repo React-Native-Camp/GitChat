d8d7c257c7b5e847933a31fa3a87a5ef414270ce
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-12-03 16:54:42 +0200

    create profile component with link to the user github profile page #35

---
+++ client/src/components/ProfileTab/index.js
@@ -0,0 +1,30 @@
+import React, {useEffect} from 'react';
+import {View, Text, Linking, TouchableOpacity} from 'react-native';
+
+import styles from './style';
+
+const ProfileTab = ({navigation}) => {
+  useEffect(() => {
+    navigation.addListener('tabPress', (e) => {
+      // Prevent default behavior
+      e.preventDefault();
+      return Linking.openURL('https://github.com');
+    });
+  }, []);
+
+  return (
+    <View style={styles.profileContainer}>
+      <Text style={styles.redirectionNote}>
+        if you are not automaticly redirected,
+      </Text>
+
+      <TouchableOpacity onPress={() => Linking.openURL('https://github.com')}>
+        <Text style={styles.githubProfileLink}> Click here to continue</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default ProfileTab;
+
+// import {View, Text, Linking, TouchableOpacity, Image} from 'react-native';
---
+++ client/src/components/ProfileTab/style.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  profileContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  redirectionNote: {
+    fontSize: 20,
+  },
+  githubProfileLink: {
+    fontSize: 18,
+    color: '#639BBB',
+  },
+});
+
+export default styles;
