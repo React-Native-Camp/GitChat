706ab53d5ba99cc8ca070d8d9b9058ba9d8feb39
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-12-09 14:10:25 +0200

    change screens back to the defaults one

--- client/screenNavigator.js
+++ client/screenNavigator.js
@@ -4,7 +4,6 @@ import {createStackNavigator} from '@react-navigation/stack';
 import LandingPage from './src/screens/LandingPage';
 import Login from './src/screens/Login';
 import AppMainPage from './src/screens/AppMainPage';
-import PopUp from './src/components/PopUp';
 
 const Stack = createStackNavigator();
 
@@ -13,26 +12,21 @@ const ScreenNavigator = () => {
     <Stack.Navigator>
       <Stack.Screen
         options={{headerShown: false}}
-        name="PopUp"
-        component={PopUp}
-      />
-      {/* <Stack.Screen
-        options={{headerShown: false}}
         name="LandingPage"
         component={LandingPage}
-      /> */}
+      />
 
-      {/* <Stack.Screen
+      <Stack.Screen
         options={{headerShown: false}}
         name="Login"
         component={Login}
-      /> */}
+      />
 
-      {/* <Stack.Screen
+      <Stack.Screen
         options={{headerShown: false}}
         name="AppMainPage"
         component={AppMainPage}
-      /> */}
+      />
     </Stack.Navigator>
   );
 };
