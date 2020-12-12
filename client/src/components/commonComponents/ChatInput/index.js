b518662ac494048f13c00e17a05cea5330e4cca9
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-12-12 15:39:32 +0200

    add socket io in client side #44

--- client/src/components/commonComponents/ChatInput/index.js
+++ client/src/components/commonComponents/ChatInput/index.js
@@ -1,39 +1,66 @@
 import React, {useState} from 'react';
 import {TextInput} from 'react-native-paper';
-import {View} from 'react-native';
+import {Text, View} from 'react-native';
 import IonIcon from 'react-native-vector-icons/dist/Ionicons';
 import MaterialCommunityIcons from 'react-native-vector-icons/dist/MaterialCommunityIcons';
 import {styles} from './style';
+import useChat from './useChat';
 
 const ChatInput = () => {
-  const [text, setText] = useState('');
   const [isTyping, setIsTyping] = useState(false);
+  const {messages, sendMessage} = useChat();
+  const [newMessage, setNewMessage] = React.useState(''); // Message to be sent
 
-  const handleInputChange = (text) => {
-    setText(text);
+  const handleNewMessageChange = (newMessage) => {
+    setNewMessage(newMessage);
 
     // to change the mic icon to send icon when the user start typing
-    text.length >= 1 ? setIsTyping(true) : setIsTyping(false);
+    newMessage.length >= 1 ? setIsTyping(true) : setIsTyping(false);
+  };
+
+  const handleSendMessage = () => {
+    sendMessage(newMessage);
+    setNewMessage('');
   };
 
   return (
-    <View style={styles.chatContainer}>
-      <TextInput
-        value={text}
-        mode="outlined"
-        placeholder="Type a message"
-        onChangeText={handleInputChange}
-        multiline={true}
-        dense={true}
-        style={styles.chatInput}
-        theme={{colors: {primary: '#639BBB', underlineColor: 'transparent'}}}
-      />
-      <IonIcon name="attach" color="#24292E" size={42} />
-      {isTyping ? (
-        <MaterialCommunityIcons name="send-circle" color="#24292E" size={40} />
-      ) : (
-        <IonIcon name="mic-outline" color="#24292E" size={40} />
-      )}
+    <View style={styles.conversation}>
+      <View style={styles.messagesContainer}>
+        {messages.map((message, i) => (
+          <Text
+            key={i}
+            style={
+              message.ownedByCurrentUser
+                ? styles.myMessage
+                : styles.receivedMessage
+            }>
+            {message.body}
+          </Text>
+        ))}
+      </View>
+      <View style={styles.chatContainer}>
+        <TextInput
+          value={newMessage}
+          mode="outlined"
+          placeholder="Type a message"
+          onChangeText={handleNewMessageChange}
+          onSubmitEditing={handleSendMessage}
+          // multiline={true}
+          dense={true}
+          style={styles.chatInput}
+          theme={{colors: {primary: '#639BBB', underlineColor: 'transparent'}}}
+        />
+        <IonIcon name="attach" color="#24292E" size={42} />
+        {isTyping ? (
+          <MaterialCommunityIcons
+            name="send-circle"
+            color="#24292E"
+            size={40}
+          />
+        ) : (
+          <IonIcon name="mic-outline" color="#24292E" size={40} />
+        )}
+      </View>
     </View>
   );
 };
--- client/src/components/commonComponents/ChatInput/style.js
+++ client/src/components/commonComponents/ChatInput/style.js
@@ -1,18 +1,39 @@
 import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
+  conversation: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  messagesContainer: {
+    flex: 1,
+    padding: 20,
+  },
   chatContainer: {
     flex: 1,
     flexDirection: 'row',
     marginLeft: 15,
     marginRight: 15,
+
     marginBottom: 20,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'flex-end',
   },
   chatInput: {
     borderColor: '#24292E',
     width: 280,
     maxHeight: 150,
+  },
+  myMessage: {
+    alignSelf: 'flex-end',
+    color: 'black',
+    backgroundColor: 'red',
+    textAlign: 'right',
+    padding: 10,
+    marginBottom: 5,
+  },
+  receivedMessage: {
+    color: 'blue',
   },
 });
